6e4cfd78f6083fff7e691484bee6a10801a8573a
Teja <saimputeja@gmail.com>
2020-04-03 12:57:54 +0530

    Second commit

--- SeleniumProject/src/main/java/Sam/S1.java
+++ SeleniumProject/src/main/java/Sam/S1.java
@@ -1,10 +1,16 @@
 package Sam;
 
+import javax.sql.rowset.WebRowSet;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class S1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.setProperty("webdriver.chrome.driver","D:\\chromedriver\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.google.co.in/?gws_rd=ssl");
 	}
 
 }
